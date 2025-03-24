75da4e67195e0a95d27c087059c23db6159f7ad3
Arthur Mesquita Redigolo <arthur.redigolo@estudante.ifms.edu.br>
2025-03-24 14:25:07 -0400

    aula 24/03

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello people!");
+});
+
+const veiculoRoutes= require("./routes/veiculoroutes");
+app.use("/veiculos",veiculoRoutes);
+
+app.listen(8000, (err) => {
+  if (err) {
+    console.log("Erro: " + JSON.stringify(err));
+  } else {
+    console.log("Aplicação rodando em localhost:8000");
+  }
+});
