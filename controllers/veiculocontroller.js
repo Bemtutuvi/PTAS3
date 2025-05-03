82711e7434d73ba548f924deb86ad35fddee2e9b
Arthur Mesquita Redigolo <arthur.redigolo@estudante.ifms.edu.br>
2025-05-03 16:19:02 -0400

    FINAL

--- controllers/veiculocontroller.js
+++ controllers/veiculocontroller.js
@@ -1,11 +1,30 @@
 const path = require("path");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
 class VeiculoController {
   static formCadastro(req, res) {
     res.sendFile(path.join(__dirname, "..", "views", "formVeiculo.html"));
   }
-  static cadastrar(req, res) {
 
+  static async cadastrar(req, res) {
+    const novoVeiculo = await prisma.veiculo.create({
+      data: {
+        placa: req.body.placa,
+        modelo: req.body.modelo,
+        ano: parseInt(req.body.ano), //essa maracutaia diferente é pra transformar isso em um numero, se chegar como texto da erro (vai q cai na prova)
+        cor: req.body.cor,
+      },
+    });
+    res.send(`O veiculo foi cadastrado sob o ID: ${novoVeiculo.id}`);
   }
-  static buscarTodos(req, res) {}
+
+  static async buscarTodos(req, res) {
+    const veiculos = await prisma.veiculo.findMany();
+
+    res.send(veiculos);
+  }
 }
+
 module.exports = VeiculoController;
