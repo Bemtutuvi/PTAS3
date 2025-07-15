7747aaba6712bfaed2be99bcb5e5282ad6551aad
Arthur Mesquita Redigolo <arthur.redigolo@estudante.ifms.edu.br>
2025-07-14 20:38:24 -0400

    finalizando

--- index.js
+++ index.js
@@ -1,44 +1,33 @@
-const express = require("express");
+const express = require ('express');
 const app = express();
+app.use(express.urlencoded({ extends: true }));
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json())
 
-const usuarioRoutes = require("./routes/usuarioroutes");
-app.use("/usuario", usuarioRoutes);
-
-const usuariocontroler = require("./controllers/usuariocontroler");
-
-app.get("/areaLogado", usuariocontroler.verificarAutenticacao, (req, res) => {
-  res.json({
-    msg:
-      "Você está logado com o ID " +
-      req.usuarioID +
-      "e pode acessar este recurso.",
-  });
+app.get("/", (req, res) =>{
+res.send("ola mundo?");
 });
 
-app.get(
-  "/areaAdmin",
-  usuariocontroler.verificarAutenticacao,
-  usuariocontroler.verificarAutenticacao,
-  (req, res) => {
-    res.json({
-      msg: "Você é um administrador!",
-    });
-  }
-);
+const veiculoRoute = require("./routes/veiculoroutes")
+app. use("/veiculos", veiculoRoute);
 
-//const veiculoRoutes= require("./routes/veiculoroutes");
-//const { verificarAutenticacao } = require("./controllers/usuariocontroller");
-//app.use("/veiculos",veiculoRoutes);
+const usuarioRoute = require("./routes/usuarioroutes");
+const usuarioController = require("./controllers/usuariocontroller");
+app. use("/usuarios", usuarioRoute);
+
+app.get("/areaLogada", usuarioController.vereficarAutentificacao, (req, res) => {
+    res.json({
+        msg:"vcesta logando com o ID: "
+        + req.usuarioId
+        + "e vc esta permitido a acessar esta area logada"
+});
+
+});
 
 app.listen(8000, (err) => {
-  console.log("Aplicação rodando em localhost:8000");
-
-  //  if (err) {
-  //  console.log("Erro: " + JSON.stringify(err));
-  //} else {
-  // console.log("Aplicação rodando em localhost:8000");
-  //}
-});
+    if (err) {
+        console.log("Erro: " + JSON.stringify(err));
+    }else{
+        console.log(`servidor rodando na porta http://localhost:${8000}`);
+    }
+})
